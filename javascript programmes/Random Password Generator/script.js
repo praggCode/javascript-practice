d4e18469a1de0767bfd47a70b57768dffff11c3a
Progyan Sen <progyansen@gmail.com>
2025-02-13 22:06:57 +0530

    Update script.js

---
+++ javascript programmes/Random Password Generator/script.js
@@ -0,0 +1,43 @@
+function genaratePassword(lenght,includeLowercase,includeUppercase,includeNumbers,includeSymbol){
+    
+    const lowercaseChacracters = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChacracters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbersChacracters = "0123456789";
+    const symbolChacracters = "!@#$%^&*()_+";
+
+    let allowedChacracters = "";
+    let password = "";
+    allowedChacracters += (includeLowercase) ? lowercaseChacracters : "";
+    allowedChacracters += (includeUppercase) ? uppercaseChacracters : "";
+    allowedChacracters += (includeNumbers) ? numbersChacracters : "";
+    allowedChacracters += (includeSymbol) ? symbolChacracters : "";
+
+    if(allowedChacracters.length === 0){
+        return "Please select at least one";
+    } 
+    if (allowedChacracters.length === 0){
+        return "Atleast one set of characters must be selected";
+    } 
+
+    for (let i = 0; i < lenght; i++){
+        const randomIndex =Math.floor(Math.random() * allowedChacracters.length);
+        password += allowedChacracters[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLenght = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbol = true;
+
+
+const password = genaratePassword(passwordLenght,
+                                  includeLowercase,     
+                                  includeUppercase,
+                                  includeNumbers,
+                                  includeSymbol);
+
+console.log(`Your password is: ${password}`);
